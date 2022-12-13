c97e856e3aee727a767ba2927c01b5c7334eee88
Nee-ma <neemapriscillar21@gmail.com>
2022-12-12 17:13:27 -0800

    push

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,15 +8,14 @@
  */
 int main(void)
 {
-	char d = '0';
+	 char c = 'a';
+	 char d = '0';
 
 	while (d <= '9')
 	{
 		putchar(d);
 		d++;
 	}
-	char c = 'a';
-
 	while (c <= 'f')
 	{
 		putchar(c);
